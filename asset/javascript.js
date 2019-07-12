d3dbcfab8d7b71fb53adf66a95827dd079f9e158
Vicky Law <vicky.law1217@gmail.com>
2019-07-12 19:26:40 -0400

    cats fixed image

--- asset/javascript.js
+++ asset/javascript.js
@@ -1,45 +1,69 @@
 $(document).ready(() => {
 
-    let api = "http://api.giphy.com/v1/gifs/search?";
+    let api = "https://api.giphy.com/v1/gifs/search?";
 
     let apiKey = "&api_key=JjKJIuZ3TirvGEdcKy2gaMVWXTNnjv2F";
 
-    let query = "&q=cats";
+    let query = "&q=smile cat";
 
-    $.ajax({
-        url: api + apiKey + query,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-    });
-    
-    
-    
+    let limit = "&limit-10";
+
+    let rating = "&rating=g";
+
+
+    let topics = ["cats", "playing cats", "fighting cats", "eating cats", "sleeping cats", "poopoo cats", "baby kitten", "wild cats", "cat walk", "jumping cats"]
+
+
     /*function setup() {
-        let url = api + apiKey + query;
+        let queryURL = api + apiKey + query + limit + rating;
         loadJSON(url.gotData);
-   
+    
     }*/
+
+    $("#cats").on("click", (() => {
+        let queryURL = "http://api.giphy.com/v1/gifs/search?q=cats%22&api_key=JjKJIuZ3TirvGEdcKy2gaMVWXTNnjv2F&limit=10&action&rating=g";
+
+        /*let stopPlayingAnimation = () => {
+            catImg.addEventListner("click", playPlayingAnimation, true);*/
+        /*$("#cats").gifplayer(() => {
+            if (img = click)
+            AnimationPlaybackEvent;
+        }
     
-    
+        })*/
 
 
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+
+        }).then((giphy) => {
+            for (let i = 0; i < giphy.data.length; i++) {
+                //let imgPauseURL = giphy.data[i].images.fixed_width_still.url;
+                let imgURL = giphy.data[i].images.fixed_width_still.url;
+                let catImg = $("<img>");
+                //($("button").on("click", ".gif"(() => {
+                //let imgPlayURL = giphy.data[i].images.fixed_width.url;
+                catImg.attr("src", imgURL);
+                catImg.attr("alt", "Cats");
+                //if (click) {
+                //    catImg.attr("src", src.replace(imgPlayURL));
+                //} else {
+                //    imgPauseURL
+
+                //}
+                /*let src = $(this).attr("src", imgURL);
+                let alt = $(this).attr("alt", "cats")*/
 
 
+              
 
+                $("#images").prepend(catImg);
+                //})));
+            }
+        });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }));
 
 });
+
